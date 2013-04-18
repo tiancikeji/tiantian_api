81db8485b814bc56e50bba4fb1fd5df2ae651b73
王雷 <wanglei@chinacache.com>
2013-04-18 20:09:36 +0800

    update

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -15,6 +15,7 @@ class Conversation < ActiveRecord::Base
     drivers.each  do | driver|
       Conversation.create(:from_id => trip.passenger_id, :to_id => driver.id, :status => STATUS_NEW, 
                           :status_desc => STATUS_NEW_DESC, :trip_id => trip.id , :content => 'a passenger want a car')
+      Conversation.push("driver-"+to_id.to_s,"conversation")
     end
   end
 
@@ -24,10 +25,10 @@ class Conversation < ActiveRecord::Base
   end
 
   # def self.push(device_id,txt)
-  def self.push
+  def self.push(alias_name,txt)
     sendno = '3323'
-    receiverType = '1' #4 broadcast 1 IMEI
-    receiverValue = "353918050081054"
+    receiverType = '3' #4 broadcast 1 IMEI
+    receiverValue = alias_name
     masterSecret = "226600dd389a295d2b90880b"
     app_key = "1fcf114a7565afe55dd79ea4"
     input = sendno + receiverType + receiverValue + masterSecret
@@ -35,7 +36,7 @@ class Conversation < ActiveRecord::Base
     uri = URI('http://api.jpush.cn:8800/sendmsg/v2/notification')
     res = Net::HTTP.post_form(uri, 'verification_code' => verificationCode,
            'sendno' => sendno,'receiver_type' => receiverType, 'receiver_value' => receiverValue,
-            'txt' => '中文测试', 'platform' => 'android', 'app_key' => app_key,
+            'txt' => txt, 'platform' => 'android', 'app_key' => app_key,
             'time_to_live' => '0')
     logger.info(res.body)
   end
